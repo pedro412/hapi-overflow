92dffb1ebca3b5052ea5d40e026ba9394c8d1664
pedro412 <pedro.alvarez412@outlook.com>
2019-11-28 12:50:05 -0600

    added handlebars, layout views, added vision and inert for handling files and or views added some basics styles to the page

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
+'use strict'
+
 const Hapi = require('@hapi/hapi')
+const handlebars = require('handlebars')
+const vision = require('@hapi/vision')
+const inert = require('@hapi/inert')
+const path = require('path')
 
 const init = async () => {
   const server = Hapi.server({
     port: process.env.port || 3000,
-    host: 'localhost'
+    host: 'localhost',
+    routes: {
+      files: {
+        relativeTo: path.join(__dirname, 'public')
+      }
+    }
   })
 
   try {
+    await server.register(inert)
+    await server.register(vision)
+
+    server.views({
+      engines: {
+        hbs: handlebars
+      },
+      relativeTo: __dirname,
+      path: 'views',
+      layout: true,
+      layoutPath: 'views'
+    })
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: (request, h) => {
+        return h.view('index', {
+          title: 'Home'
+        })
+      }
+    })
+
+    server.route({
+      method: 'GET',
+      path: '/{param*}',
+      handler: {
+        directory: {
+          path: '.',
+          index: ['index.html']
+        }
+      }
+    })
+
     await server.start()
   } catch (error) {
     console.log(error)
     process.exit(1)
   }
-
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (reques, h) => {
-      return 'Hello world!'
-    }
-  })
 
   console.log(`Server running on ${server.info.uri}`)
 }
